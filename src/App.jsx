ccfc96ac0cf42817bf1bdccce8eeba4cce1e70aa
DiodeCN <1069523953@qq.com>
2023-05-23 05:56:03 +0800

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    const date = new Date();
+    setTitle(date.toDateString());
+
+    const fetchNews = async () => {
+      try {
+        const response = await fetch(
+          `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+        );
+        const data = await response.json();
+        const news = data.articles[0];
+        setContent(news.title + "\n" + news.description);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchNews();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '297mm',
+      width: '210mm',
+      margin: '0 auto',
+      padding: '25mm',
+      boxSizing: 'border-box',
+      background: 'white',
+    }}>
+      <h2 style={{textAlign: 'center'}}>{title}</h2>
+      <div style={{
+        marginTop: '20px',
+        textAlign: 'justify'
+      }}>{content}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
