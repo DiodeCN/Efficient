0f319586f6c2df4c12424e31ad17096b38fe9808
DiodeCN <1069523953@qq.com>
2023-05-23 21:45:55 +0800

    2

--- fronted/src/App.tsx
+++ fronted/src/App.tsx
@@ -1,20 +1,10 @@
 import { TextField, Typography, Box } from '@mui/material';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <Typography variant="h1" align="center">
         Vite + React
       </Typography>
