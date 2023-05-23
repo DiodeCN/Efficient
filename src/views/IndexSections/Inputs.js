fe018b3698683a4be17e2c370b33a338c2440dfa
DiodeCN <1069523953@qq.com>
2023-05-23 17:12:21 +0800

    3

--- src/views/IndexSections/Inputs.js
+++ src/views/IndexSections/Inputs.js
@@ -104,106 +104,14 @@ class Inputs extends React.Component {
                 <FormGroup className="has-danger">
                   <Input
                     className="is-invalid"
-                    placeholder="Error Input"
+                    placeholder="Error"
                     type="email"
                   />
                 </FormGroup>
               </Col>
             </Row>
           </Container>
-          <div className="py-5 bg-secondary">
-            <Container>
-              {/* Inputs (alternative) */}
-              <div className="mb-3">
-                <small className="text-uppercase font-weight-bold">
-                  Form controls (alternative)
-                </small>
-              </div>
-              <Row>
-                <Col lg="4" sm="6">
-                  <FormGroup>
-                    <Input
-                      className="form-control-alternative"
-                      placeholder="Regular"
-                      type="text"
-                    />
-                  </FormGroup>
-                  <FormGroup
-                    className={classnames({
-                      focused: this.state.searchAltFocused
-                    })}
-                  >
-                    <InputGroup className="input-group-alternative mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="ni ni-zoom-split-in" />
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        placeholder="Search"
-                        type="text"
-                        onFocus={(e) =>
-                          this.setState({ searchAltFocused: true })
-                        }
-                        onBlur={(e) =>
-                          this.setState({ searchAltFocused: false })
-                        }
-                      />
-                    </InputGroup>
-                  </FormGroup>
-                </Col>
-                <Col lg="4" sm="6">
-                  <FormGroup>
-                    <Input
-                      className="form-control-alternative"
-                      disabled
-                      placeholder="Regular"
-                      type="text"
-                    />
-                  </FormGroup>
-                  <FormGroup
-                    className={classnames({
-                      focused: this.state.birthdayAltFocused
-                    })}
-                  >
-                    <InputGroup className="input-group-alternative mb-4">
-                      <Input
-                        placeholder="Birthday"
-                        type="text"
-                        onFocus={(e) =>
-                          this.setState({ birthdayAltFocused: true })
-                        }
-                        onBlur={(e) =>
-                          this.setState({ birthdayAltFocused: false })
-                        }
-                      />
-                      <InputGroupAddon addonType="append">
-                        <InputGroupText>
-                          <i className="ni ni-zoom-split-in" />
-                        </InputGroupText>
-                      </InputGroupAddon>
-                    </InputGroup>
-                  </FormGroup>
-                </Col>
-                <Col lg="4" sm="6">
-                  <FormGroup className="has-success">
-                    <Input
-                      className="form-control-alternative is-valid"
-                      placeholder="Success"
-                      type="text"
-                    />
-                  </FormGroup>
-                  <FormGroup className="has-danger">
-                    <Input
-                      className="form-control-alternative is-invalid"
-                      placeholder="Error Input"
-                      type="email"
-                    />
-                  </FormGroup>
-                </Col>
-              </Row>
-            </Container>
-          </div>
+         
         </section>
       </>
     );
